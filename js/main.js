7ab29c95890bd1bcf85baf793f69db417b860e19
theamanjs <amanjotsingh1599@gmail.com>
2019-10-02 12:23:07 +0530

    Judgement of winning is done and removed some UI bugs

--- js/main.js
+++ js/main.js
@@ -5,42 +5,90 @@ function GameStatus(){
     this.myScore = 0;
     this.opponentScore = 0;
 } 
+const gameStatus = new GameStatus();
+
  
 function MyMove(selection){
     this.selection = selection;
 }
 
+function OpponentMove(){
+    this.selection = ['stone', 'paper', 'scissors'];
+}
+
+let bufferSelection = {
+    mySelection : '',
+    opponentSelection : '',
+    draw(){
+        console.log(result)
+    }
+}
+~(function checkSelection(){
+let check = setInterval(() => {
+    if(bufferSelection.mySelection && bufferSelection.opponentSelection){
+        judgement();
+        showResult();
+        clearInterval(check);
+    }
+}, 100);
+})();
+
+
+let limit = (max, min) => Math.random() * (max - min) + min;
+function selectionOpponent(){
+    let opponentMove = new OpponentMove();
+    let randomNumber = Math.round(limit(2,0));
+    // console.log(randomNumber);
+    console.log(opponentMove.selection[randomNumber]);
+    bufferSelection.opponentSelection = opponentMove.selection[randomNumber];
+    // bufferSelection.draw();
+    document.querySelector("#opponentStatus").setAttribute('src','./images/tick.png');
+    opponentDisplay(opponentMove.selection[randomNumber]);
+} 
+setTimeout(selectionOpponent, Math.round(limit(3000,500)));
+// console.log(buff)
+
+function opponentDisplay(selection){
+    let image = './images/' + selection + '.svg';
+    document.querySelector("#opponentStatus").setAttribute('src',image);
+    // console.log(image);
+}
+
+
 function selectedItem(selection){
     let move = new MyMove(selection);
-    let hidePaper = document.querySelector(".paper").style ; 
-    let hideScissors = document.querySelector(".scissors").style ;
-    let hideStone = document.querySelector(".stone").style ;
-    console.log(move.selection);
+    let Paper = document.querySelector(".paper").style ; 
+    let Scissors = document.querySelector(".scissors").style ;
+    let Stone = document.querySelector(".stone").style ;
+    // console.log(move.selection);
+    bufferSelection.mySelection = move.selection;
+    // bufferSelection.draw();
+
     if(move.selection === "stone"){
-        hidePaper.display = 'none';
-        hideScissors.display = 'none';
+        Paper.display = 'none';
+        Scissors.display = 'none';
     }
     if(move.selection === "paper"){
-        hideStone.display = 'none';
-        hideScissors.display = 'none';
+        Stone.display = 'none';
+        Scissors.display = 'none';
     }
     if(move.selection === "scissors"){
-        hidePaper.display = 'none';
-        hideStone.display = 'none';
+        Paper.display = 'none';
+        Stone.display = 'none';
     }
     document.querySelector(".status-my").innerText = general.processing.proponent.made;
 
 
-    console.log(move.selection);
+    // console.log(move.selection);
 }
 
-const gameStatus = new GameStatus();
 
 const gameInfo = {
     general:{
         resultTag:{
             won: "You Won",
             loss: "You Loss",
+            tie: "Tie",
             null: ""
         },
         processing:{
@@ -104,7 +152,75 @@ function checkScore(){
     
 }
 
+function judgement(){
+    if(bufferSelection.mySelection === 'stone' && bufferSelection.opponentSelection === 'stone'){
+        console.log(general.resultTag.tie);
+        document.querySelector("#result").innerText = general.resultTag.tie;
+        return;
+    }
+    if(bufferSelection.mySelection === 'stone' && bufferSelection.opponentSelection === 'scissors'){
+        console.log(general.resultTag.won);
+        gameStatus.myScore++;
+        document.querySelector("#result").innerText = general.resultTag.won;
+        checkScore();
+        return;
+    }
+    if(bufferSelection.mySelection === 'stone' && bufferSelection.opponentSelection === 'paper'){
+        console.log(general.resultTag.loss);
+        gameStatus.opponentScore++;
+        document.querySelector("#result").innerText = general.resultTag.loss;
+        checkScore();
+        return;
+    }
+    if(bufferSelection.mySelection === 'paper' && bufferSelection.opponentSelection === 'paper'){
+        console.log(general.resultTag.tie);
+        document.querySelector("#result").innerText = general.resultTag.tie;
+        checkScore();
+        return;
+    }
+    if(bufferSelection.mySelection === 'paper' && bufferSelection.opponentSelection === 'stone'){
+        console.log(general.resultTag.won);
+        gameStatus.myScore++;
+        document.querySelector("#result").innerText = general.resultTag.won;
+        checkScore();
+        return;
+    }
+    if(bufferSelection.mySelection === 'paper' && bufferSelection.opponentSelection === 'scissors'){
+        console.log(general.resultTag.loss);
+        gameStatus.opponentScore++;
+        document.querySelector("#result").innerText = general.resultTag.loss;
+        checkScore();
+        return;
+    }
+    if(bufferSelection.mySelection === 'scissors' && bufferSelection.opponentSelection === 'scissors'){
+        console.log(general.resultTag.tie);
+        document.querySelector("#result").innerText = general.resultTag.tie;
+        checkScore();
+        return;
+    }
+    if(bufferSelection.mySelection === 'scissors' && bufferSelection.opponentSelection === 'stone'){
+        console.log(general.resultTag.loss);
+        gameStatus.opponentScore++;
+        document.querySelector("#result").innerText = general.resultTag.loss;
+        checkScore();
+        return;
+    }
+    if(bufferSelection.mySelection === 'scissors' && bufferSelection.opponentSelection === 'paper'){
+        console.log(general.resultTag.won);
+        gameStatus.myScore++;
+        document.querySelector("#result").innerText = general.resultTag.won;
+        checkScore();
+        return;
+    }
+    // console.log("My ",gameStatus.myScore);
+    // console.log("Opponent ",gameStatus.opponentScore);
+   
+}
 
-
-
+function showResult(){
+    setTimeout(() => {
+            document.querySelector("#result").innerText = general.resultTag.null;
+        }, 3000);
+        // clearInterval(showResult);
+}
 // console.log(gameInfo.user.round);
